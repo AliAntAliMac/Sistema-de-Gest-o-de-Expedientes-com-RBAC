06afebc5afd53048a57efa158e811bdf27892abf
AAAM <aaam@gmail.com>
2026-08-24 11:52:05 +0200

    Primeiro commit do projecto

---
+++ src/test/java/com/grupoDeTrabalho/gestao_espedientes/GestaoEspedientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoDeTrabalho.gestao_espedientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEspedientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
